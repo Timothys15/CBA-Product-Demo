bc9e890d8614a325a47ac39e0a13434378e235be
Daniel English <dan.english3@gmail.com>
2019-04-24 15:19:16 +1200

    added in MonoDB connection

--- product_test_backend/server.js
+++ product_test_backend/server.js
@@ -1,13 +1,30 @@
 console.log('SERVER IS STARTING YO');
 
-var express = require('express');
+const express = require('express');
+const app = express();
+const server = app.listen(3000, listening);
+const router = express.Router(); // Get express's router functions
+const bodyParser = require('body-parser');
 
-var app = express ();
-
-var server = app.listen(3000, listening);
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://localhost:27017'; // DB Connection URL
+const dbName = 'cba';// Database Name
 
 function listening() {
     console.log("listening....");
 }
 
+app.use(bodyParser.json()); // Parse input text to JSON
+app.use(bodyParser.urlencoded({ extended: true })); // Ensure proper/safe URL encoding
 app.use(express.static('public'));
+
+//DB connect
+MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to MongoDB");
+
+    const db = client.db(dbName);
+
+    client.close();
+});
