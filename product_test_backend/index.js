fa51fdc38240e65e4089413247abe2d497212b24
Tim <tim.serrano@live.com>
2019-04-29 22:12:57 +1200

    Add Document Service implemented
    -No String tokenization yet
    -Load button for each collection

--- product_test_backend/index.js
+++ product_test_backend/index.js
@@ -3,48 +3,53 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 var assert = require('assert');
 var dbName = 'CBA_Project';     // Database Name, change this to the name of your local MongoDB database
+var collectionOne = 'Create_Model_Collection';
+var collectionTwo = 'add_Document_Collection';
 var url = `mongodb://localhost:27017/${dbName}`;
-var ObjectID = require('mongodb').ObjectID;
 
 var app = express();
-var item;
-
-// app.configure(function(){
-//     app.use(express.bodyParser());
-//     app.use(app.router);
-// });
 
 app.use(bodyParser.json()); // Parse input text to JSON
 app.use(bodyParser.urlencoded({ extended: true })); // Ensure proper/safe URL encoding
 
-app.get('/get-data', function (req, res) {
+app.get('/get-service1-data', function (req, res) {
+    //The data from the MongoDB is loaded into data_array
 
-    var inputId = { _id: new ObjectID(req.query.id)};
-
-    //console.log("id.model_name + ' ' + id.timestamp");
-
-    MongoClient.connect(url, { useNewUrlParser: true }, function (err, client){
+    MongoClient.connect(url, {useNewUrlParser: true }, function (err, client){
         assert.equal(null, err);
-        var resultArray = [];
+        var data_array = [];
         var db = client.db(dbName);
-        console.log("db: " + db.databaseName);
 
-        db.collection("testing_database").find(inputId).toArray(function(err, result) {
+        db.collection(`${collectionOne}`).find().toArray(function(err, result) {
             if(err) throw err;
             console.log(result.model_name, result);
             client.close();
             res.send(result);
         });
     });
-    //res.redirect('/');
 })
+
+app.get('/get-service2-data', function (req, res) {
+    MongoClient.connect(url, {useNewUrlParser: true }, function (err, client){
+        assert.equal(null, err);
+        var data_array = [];
+        var db = client.db(dbName);
+
+        db.collection(`${collectionTwo}`).find().toArray(function(err, result) {
+            if(err) throw err;
+            console.log(result.model_id, result);
+            client.close();
+            res.send(result);
+        });
+    });
+})
+
 
 //inserting into MongoDB must be in the curly braces of the app.post
 //Accepts the inputs from create a model form box
 app.post('/create', function(req, res){
     console.log("The information entered is: ", req.body);
-    
-    item = {
+    var item = {
         model_name: req.body.model_name,
         timestamp: req.body.timestamp
     };
@@ -55,23 +60,37 @@ app.post('/create', function(req, res){
         const db = client.db(dbName);
 
         //This creates a collection if it does not already exist
-        db.collection('testing_database').insertOne(item, function(err, result) {
+        db.collection(`${collectionOne}`).insertOne(item, function(err, result) {
             assert.equal(null, err);
-            console.log('Item has been successfully inserted');
-            
-            //var objectId = item._id;
-            //console.log(item.model_name+' id is: '+objectId);
-        });
+            console.log(`Item has been successfully inserted into ${collectionOne}.`);
 
-        client.close();
+        });
     });
 
     res.redirect('/');
 });
 
-function getDetails() {
-    var userInput = document.getElementById('model_name').value;
-    alert(userInput);
-}
+app.post('/add-document', function(req, res){
+    console.log("The information entered is: ", req.body);
+    var item = {
+        model_id: req.body.model_id,
+        plain_text: req.body.plain_text
+    };
+
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, client){
+        assert.equal(null, err);
+
+        const db = client.db(dbName);
+
+        //This creates a collection if it does not already exist
+        db.collection(`${collectionTwo}`).insertOne(item, function(err, result) {
+            assert.equal(null, err);
+            console.log(`Item has been successfully inserted into ${collectionTwo}.`);
+
+        });
+    });
+
+    res.redirect('/');
+});
 
 module.exports = app;
